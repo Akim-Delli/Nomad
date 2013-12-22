76c340870e270b76d8a9144ed3f97103c308cede
Akim Delli <akim@ipro.us>
2013-12-22 18:04:48 -0500

    reset password functionnality

--- app.js
+++ app.js
@@ -81,4 +81,19 @@ app.post('/forgotpassword', function(req,res) {
 	});
 });
 
+app.get('/resetPassword', function(req, res) {
+	var accountId = req.param('account', null);
+	res.render('resetPassword.jade', {locals:{accountId:accountId}});
+});
+
+app.post('/resetPassword', function(req, res) {
+	var accountId = req.param('accountId', null);
+	var password = req.param('password', null);
+	if(null !== accountId && null !== password ) {
+		Account.changePassword(accountId, password);
+	}
+	res.render('resetPasswordSuccess.jade');
+});
+
+
 app.listen(8080);
