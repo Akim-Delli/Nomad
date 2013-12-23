3d9296a8e3f278cdc800077c260b26cdf0122f75
Akim Delli <akim@ipro.us>
2013-12-22 19:52:34 -0500

    nodemailer setup

--- app.js
+++ app.js
@@ -4,39 +4,32 @@
 
 var express = require('express');
 var app = express();
+var nodemailer = require('nodemailer');
+var MemoryStore = require('connect').session.MemoryStore;
+
+// Import the data layer
+var mongoose = require('mongoose');
+
+var config = {
+	mail: require('./config/mail')
+};
+
+// Import the accounts
+var Account = require('./models/Account')(config,  mongoose, nodemailer);
 
 app.configure(function(){
 	app.set('view engine','jade');
 	app.use(express.static(__dirname + '/public'));
+	app.use(express.limit('1mb'));
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session(
+		{secret: "Nomad secret key", store: new MemoryStore()}));
+	mongoose.connect('mongodb://localhost/nodebackbone');
 });
 
 app.get('/', function(req, res){
 	res.render("index.jade", {layout: false});
-});
-
-app.get('/account/authenticated', function(req, res){
-	if ( req.session.loggedIn ) {
-		res.send(200);
-	} else {
-		res.send(401);
-	}
-});
-
-app.post('/register', function(req,res) {
-	var firstname = req.param('firstName', '');
-	var lastName = req.param('lastName','');
-	var email = req.param('email', null);
-	var password = req.param('password', null);
-
-	if (null === email || null === password) {
-		res.send(400);
-		return;
-	}
-
-	//!! registration is going to get fired off and handled
-	//!!even after the user received an OK response from the server.
-	Account.register(email, password, firstName, lastName);
-	res.send(200);
 });
 
 app.post('/login', function(req,res) {
@@ -60,6 +53,32 @@ app.post('/login', function(req,res) {
 		res.send(200);
 	});
 	res.send(200);
+});
+
+app.post('/register', function(req,res) {
+	var firstname = req.param('firstName', '');
+	var lastName = req.param('lastName','');
+	var email = req.param('email', null);
+	var password = req.param('password', null);
+
+	if (null === email || null === password) {
+		res.send(400);
+		return;
+	}
+
+	//!! registration is going to get fired off and handled
+	//!!even after the user received an OK response from the server.
+	Account.register(email, password, firstName, lastName);
+	res.send(200);
+});
+
+
+app.get('/account/authenticated', function(req, res){
+	if ( req.session.loggedIn ) {
+		res.send(200);
+	} else {
+		res.send(401);
+	}
 });
 
 app.post('/forgotpassword', function(req,res) {
